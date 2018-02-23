024df2aa57665fa51650681f01aa841f5f06df49
Norman Chan <slowmanchan@gmail.com>
2018-02-23 12:21:50 -0500

    Revert "Revert "Favorites feature""

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,7 +1,11 @@
-import axios from 'axios'
+import axios from 'axios';
+import _ from 'lodash';
 
 export const FETCH_MOVIES = 'FETCH_MOVIES';
 export const FETCH_MOVIE = 'FETCH_MOVIE';
+export const ADD_FAVORITE = 'ADD_FAVORITE';
+export const DELETE_FAVORITE = 'DELETE_FAVORITE';
+export const FETCH_FAVORITES = 'FETCH_FAVORITES';
 
 const ROOT_URL = 'http://www.omdbapi.com/?';
 const API_KEY = 'apikey=eda26e6d';
@@ -22,5 +26,35 @@ export function fetchMovie(id) {
 	return {
 		type: FETCH_MOVIE,
 		payload: request
+	}
+}
+
+export function addFavorite(movie) {
+	window.localStorage.setItem('favorites ' + movie.Title, JSON.stringify(movie))
+	
+	return {
+		type: ADD_FAVORITE,
+		payload: movie
+	}
+}
+
+export function deleteFavorite(movie) {
+	window.localStorage.removeItem(movie.Title);
+	
+	return {
+		type: DELETE_FAVORITE,
+		payload: movie
+	}
+}
+
+export function fetchFavorites() {
+	var values = _.values(localStorage);
+	var valuesParsed = values.map((value) => {
+	  return JSON.parse(value)
+	})
+	
+	return {
+		type: FETCH_FAVORITES,
+		payload: valuesParsed
 	}
 }
